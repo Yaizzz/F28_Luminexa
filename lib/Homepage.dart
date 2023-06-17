e43b1a064508c7af1448ebd979dcdcf72ed8dee7
Akk <kadir2257@gmail.com>
2023-06-17 11:28:01 +0300

    add home page and navigationbar

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,55 +1,117 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 void main() {
-  runApp(homePage());
+  runApp(MyApp());
 }
 
-class homePage extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
-      title: 'Home Page',
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          title: Center(
-            child: const Text(
-              'KATIL',
-              style: TextStyle(color: Colors.red),
-            ),
-          ),
-        ),
-        // SONSUZ LIST VIEW
-        body: FractionallySizedBox(
-          heightFactor: 0.15,
-          child: Container(
-            color: Colors.blue,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text('KATIL'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 1,
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
+              itemCount: 10,
               itemBuilder: (context, index) {
                 return Container(
-                  width: 120,
-                  color: index % 2 == 0 ? Colors.red : Colors.green,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.red),
+                    shape: BoxShape.circle,
+                    color: Colors.transparent,
+                  ),
+                  width: MediaQuery.of(context).size.width / 5,
+                  margin: EdgeInsets.all(8.0),
                   child: Center(
                     child: Text(
-                      '$index',
-                      style: TextStyle(color: Colors.white),
+                      'katagoriler - $index',
+                      style: TextStyle(color: Colors.red),
                     ),
                   ),
                 );
               },
             ),
           ),
-        ),
+          Expanded(
+            flex: 1,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.red),
+                    borderRadius: BorderRadius.circular(100),
+                    color: Colors.transparent,
+                  ),
+                  width: MediaQuery.of(context).size.width / 5,
+                  margin: EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Text(
+                      'duyurular - $index',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              color: Colors.transparent,
+              child: ListView.builder(
+                itemCount: 20,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('DETAY SAYFALARI $index'),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Anasayfa',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Arama',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: 'Takvim',
+          ),
+        ],
       ),
     );
   }
 }
-
-
 
 
 
